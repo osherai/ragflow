2dfc8c616e722bcf28ee37e3c7b42c5e15046e64
Nicholas-Dunstan <nicholas@osher.com.au>
2025-10-13 17:28:43 +1000

    Remove large logo overlay from embed chat interface

--- web/src/components/embed-container.tsx
+++ web/src/components/embed-container.tsx
@@ -20,10 +20,11 @@ export function EmbedContainer({
 
   return (
     <section className="h-[100vh] flex justify-center items-center">
-      <div className="w-40 flex gap-2 absolute left-3 top-12 items-center">
+      {/* Large logo overlay commented out to avoid covering chat interface */}
+      {/* <div className="w-40 flex gap-2 absolute left-3 top-12 items-center">
         <img src="/logo.svg" alt="" />
         <span className="text-2xl font-bold">{appConf.appName}</span>
-      </div>
+      </div> */}
       <div className=" w-[80vw] border rounded-lg">
         <div className="flex justify-between items-center border-b p-3">
           <div className="flex gap-2 items-center">
